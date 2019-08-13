996a677868030f92ae01f7509dae8e41cc803c3e
kerianVaraine <chambervulgarus@gmail.com>
2019-08-13 17:56:42 +1200

    more

--- app/src/main/java/com/info305/milestone2/MainActivity.java
+++ app/src/main/java/com/info305/milestone2/MainActivity.java
@@ -8,6 +8,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.TextView;
 
 //graphView Stuff
@@ -22,10 +23,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     private float x, y, z;
 
-    private TextView currentX, currentY, currentZ;
-
-
-
+    private TextView currentX, currentY, currentZ, currentMag;
+//Graph stuff
+private final Handler mHandler = new Handler();
+    private Runnable mTimer;
+    private double graphLastXValue = 5d;
+    private LineGraphSeries<DataPoint> mSeries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +41,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
             accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
             sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-
-
-
     }
 
     //INITS
@@ -48,18 +48,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     currentX = findViewById(R.id.currentX);
     currentY = findViewById(R.id.currentY);
         currentZ = findViewById(R.id.currentZ);
+        currentMag = findViewById(R.id.currentMag);
 
     //GraphView Stuff
         GraphView graph = (GraphView) findViewById(R.id.graph);
 
+        // first mSeries is a line
+        mSeries = new LineGraphSeries<>();
+        mSeries.setDrawDataPoints(true);
+        mSeries.setDrawBackground(true);
+        graph.addSeries(mSeries);
     }
 
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-    }
+
 
     @Override
     public void onPause(){
@@ -74,8 +76,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         y = accelValues[1];
         z = accelValues[2];
         displayValues();
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mTimer = new Runnable() {
+            @Override
+            public void run() {
+                graphLastXValue += 0.25d;
+                mHandler.postDelayed(this, 330);
+                mSeries.appendData(new DataPoint(graphLastXValue, getX(), true, 22);
+            }
+        };
+        mHandler.postDelayed(mTimer, 1500);
 
     }
+
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
@@ -87,6 +109,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         currentX.setText(Float.toString(x));
         currentY.setText(Float.toString(y));
         currentZ.setText(Float.toString(z));
+        currentMag.setText(Double.toString(calcMagnitude()));
+    }
 
+    public double calcMagnitude (){
+
+        return java.lang.Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
     }
 }
